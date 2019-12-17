b958c69221db2501ecd697ef64f01aff6e093d28
deama <deama@mail.com>
2019-12-17 09:21:13 +0000

    build.sh removes corresponding docker processes

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+docker rm -f flask-host
+docker rm -f flask-number
+docker rm -f flask-letter
+docker rm -f flask-sequence
+
 docker build -t flask-host ./flask_host
 docker build -t flask-number ./flask_generator_number
 docker build -t flask-letter ./flask_generator_letter
