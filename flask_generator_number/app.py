8c9f6071e99627a28593a8c898d0643c0ab8a952
deama <deama@mail.com>
2020-01-10 10:04:40 +0000

    just have the jenkins file on this branch

--- flask_generator_number/app.py
+++
@@ -1,9 +0,0 @@
-from application import app
-
-
-
-
-if __name__ == "__main__":
-    app.run()
-
-from application import routes
--- flask_generator_number/application/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import request, render_template
-from application import app
-import requests
-import random
-
-@app.route("/getRandomNumber", methods=["POST"])
-def getNumber():
-    return {"number":random.randint(0, 9)}
